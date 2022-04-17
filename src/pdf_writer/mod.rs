95fc1fc79e75cb8b00df2f0564ad1948a177dbdc
Jacob V. Gardner <jacob.gardner@wonderlic.com>
2022-04-16 21:09:40 -0500

    Refactor some stuff. Add some unit tests

--- src/pdf_writer/mod.rs
+++ src/pdf_writer/mod.rs
@@ -1,22 +1,19 @@
 use std::{fs::File, io::BufWriter};
 
-use lazy_static::lazy_static;
 use printpdf::{
     lopdf, IndirectFontRef, Line, Mm, PdfDocument, PdfDocumentReference, PdfLayerIndex,
-    PdfLayerReference, PdfPageIndex, Point, Pt, Px, Rgb, Svg, SvgTransform, TextMatrix, calculate_points_for_circle,
+    PdfLayerReference, PdfPageIndex, Point, Pt, Rgb, TextMatrix, calculate_points_for_circle, Color,
 };
-use regex::Regex;
 use skia_safe::Typeface;
 use tracing::{instrument, span, Level};
+
+mod svg;
 
 use crate::{
     fonts::{find_font_index_by_style, FONTS},
     line_metric::LineMetric,
-    rich_text::{FontWeight, RichText, RichTextStyle},
-    text_layout::TextLayout,
+    rich_text::{RichText},
 };
-
-const C: f64 = 0.551915024494;
 
 pub struct PdfWriter {
     dimensions: (Mm, Mm),
@@ -28,6 +25,7 @@ pub struct PdfWriter {
 impl PdfWriter {
     #[instrument(name = "Create PDF Context")]
     pub fn new() -> Self {
+        
         // A4 Page dimensions
         let dimensions = (Mm(210.), Mm(297.));
 
@@ -84,29 +82,6 @@ pub struct PageWriter<'a> {
     writer: &'a PdfWriter,
 }
 
-pub fn svg_to_pt(svg_unit: &str) -> Pt {
-    lazy_static! {
-        static ref RE: Regex =
-            Regex::new(r"^(?i)(?P<quantity>[\.\d]+)(?P<units>cm|mm|Q|in|pc|pt|px)?$").unwrap();
-    }
-
-    let caps = RE.captures(svg_unit).unwrap();
-    let quantity: f64 = caps.name("quantity").unwrap().as_str().parse().unwrap();
-    let units = caps.name("units").map_or("px", |u| u.as_str());
-
-    // TODO: Verify that these are correct, probably
-    match units.to_lowercase().as_str() {
-        "px" => Mm(quantity * (25.4 / 300.)).into(),
-        "mm" => Mm(quantity).into(),
-        "cm" => Mm(quantity * 10.0).into(),
-        "pt" => Pt(quantity),
-        "q" => Mm(quantity * 40.).into(),
-        "in" => Mm(quantity / 25.4).into(),
-        "pc" => Mm(quantity / 25.4 * 6.).into(),
-        _ => panic!("Unknown unit types {units}"),
-    }
-}
-
 impl<'a> PageWriter<'a> {
     fn new(writer: &'a PdfWriter, page_index: PdfPageIndex, layer_index: PdfLayerIndex) -> Self {
         Self {
@@ -121,108 +96,6 @@ impl<'a> PageWriter<'a> {
             .doc
             .get_page(self.page_index)
             .get_layer(self.layer_index)
-    }
-
-    pub fn draw_svg(&self, start: Point, svg_text: &str) -> &Self {
-        // let string_to_path_svg = tree.to_string(&usvg::XmlOptions::default());
-        // let text_node = tree.node_by_id("text");
-        let current_layer = self.get_current_layer();
-
-        let svg = Svg::parse(&svg_text).unwrap();
-
-        let svg_ref = svg.into_xobject(&current_layer);
-
-        svg_ref.add_to_layer(
-            &current_layer,
-            SvgTransform {
-                translate_x: Some(start.x),
-                translate_y: Some(start.y),
-                scale_x: Some(1.0),
-                scale_y: Some(1.0),
-                ..Default::default()
-            },
-        );
-
-        let doc = roxmltree::Document::parse(&svg_text).unwrap();
-        for node in doc.descendants().filter(|n| n.tag_name().name() == "text") {
-            let t = node.tag_name();
-
-            // The SVG units are in px by default, and we're assuming that here.
-            //  We have to convert that to Pt which printpdf has a method for, but it
-            //  takes a usize, but the svg pixels can be fractions so... we replicate that
-            //  here
-
-            // TODO: Document/warn that we currently do NOT support text nodes in nested transformations
-            //  OR add support for it.
-            // TODO: Extract px => pt conversion
-            // TODO: Support other unit types OR throw an error if provided
-            // TODO: Document/warn about unsupported attributes
-            let x = svg_to_pt(node.attribute("x").unwrap_or("0"));
-            let y = svg_to_pt(node.attribute("y").unwrap_or("0"));
-            let weight = FontWeight::from(node.attribute("font-weight").unwrap_or("regular"));
-            let font_style = node.attribute("font-style").unwrap_or("normal");
-            let is_italic = font_style.to_lowercase() == "italic";
-            let font_size = svg_to_pt(node.attribute("font-size").unwrap_or("12"));
-            let fill =
-                color_processing::Color::new_string(node.attribute("fill").unwrap_or("#000000"))
-                    .unwrap()
-                    .get_rgba();
-            let anchor = node.attribute("text-anchor").unwrap_or("start");
-            let font_stack = node.attribute("font-family").unwrap_or("sans-serif");
-
-            let preferred_fonts: Vec<_> = font_stack.split(",").map(|f| f.trim()).collect();
-
-            println!("Font Stack: {:?}", preferred_fonts);
-            // We want to find a font in the stack that matches up to a loaded skia/pdf typeface.
-            //   If we don't find one, default to the first typeface, probably?
-
-            let is_centered = anchor.to_lowercase() == "middle";
-
-            // Once we have the correct typeface found, we should be able to use Skia to get the line_metrics
-            //  for the string and compute what we need to compute for center/end alignment and vertical alignment
-            // TODO: This^^
-
-            let layout = TextLayout::new();
-            if !node.children().all(|n| n.is_text()) {
-                panic!("For <text>, we only support all text child nodes for now");
-            }
-
-            let node_text = node.text().unwrap().trim();
-            let rich = RichText::new(
-                node_text,
-                RichTextStyle {
-                    font_size,
-                    weight,
-                    italic: is_italic,
-                    color: (fill.0 as f32, fill.1 as f32, fill.2 as f32),
-                },
-            );
-
-            let paragraph = layout.compute_paragraph_layout(&rich, Pt(1000.0));
-
-            assert_eq!(paragraph.line_metrics.len(), 1);
-
-            let line_metric = paragraph.line_metrics.first().unwrap();
-
-            println!("{:?}", line_metric);
-
-            // current_layer.set_text_matrix(TextMatrix::Translate(start.x + Pt(x), start.y + Pt(y)));
-            current_layer.set_text_matrix(TextMatrix::Translate(
-                start.x + x.into() - line_metric.width / 2.,
-                start.y + y.into() - ((line_metric.ascent - line_metric.descent) / 2.), // - ((line_metric.ascent - line_metric.baseline) / 2.),
-            ));
-
-            let font_idx = find_font_index_by_style(weight, is_italic);
-            let current_font = &self.writer.fonts[font_idx];
-
-            current_layer.set_font(current_font, Pt::from(font_size).0);
-            current_layer
-                .set_fill_color(printpdf::Color::Rgb(Rgb::new(fill.0, fill.1, fill.2, None)));
-
-            PageWriter::write_text(&current_layer, &node_text, &layout.typeface);
-        }
-
-        self
     }
 
     pub fn draw_rect(&self, start: Point, end: Point, border_radius: Option<Pt>) -> &Self {
@@ -252,7 +125,6 @@ impl<'a> PageWriter<'a> {
             points.extend(circle_points[8..12].iter().map(|&(pt, b)| (Point { x: pt.x + border_radius + start.x, y: pt.y + border_radius + end.y}, b)));
             
             points
-
         } else {
             vec![
                 (Point { x: start.x, y: start.y, }, false),
@@ -262,7 +134,7 @@ impl<'a> PageWriter<'a> {
             ]
         };
 
-        current_layer.set_fill_color(printpdf::Color::Rgb(Rgb::new(0.8, 1., 0.8, None)));
+        current_layer.set_fill_color(Color::Rgb(Rgb::new(0.8, 1., 0.8, None)));
         let line = Line {
             points,
             is_closed: true,
@@ -339,7 +211,7 @@ impl<'a> PageWriter<'a> {
 
                 current_layer.set_font(current_font, current_style.font_size.0);
                 let clr = current_style.color;
-                current_layer.set_fill_color(printpdf::Color::Rgb(Rgb::new(
+                current_layer.set_fill_color(Color::Rgb(Rgb::new(
                     clr.0 as f64,
                     clr.1 as f64,
                     clr.2 as f64,
---
+++ src/pdf_writer/svg.rs
@@ -0,0 +1,195 @@
+use lazy_static::lazy_static;
+use printpdf::{Color, Mm, Point, Pt, Rgb, Svg, SvgTransform, TextMatrix};
+use regex::Regex;
+
+use crate::{
+    fonts::find_font_index_by_style,
+    rich_text::{FontWeight, RichText, RichTextStyle},
+    text_layout::TextLayout,
+};
+
+use super::PageWriter;
+
+fn svg_to_pt(svg_unit: &str) -> Pt {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"^(?i)(?P<quantity>[\.\d]+)(?P<units>\D+)?$").unwrap();
+    }
+
+    let caps = RE.captures(svg_unit).unwrap();
+    let quantity: f64 = caps.name("quantity").unwrap().as_str().parse().unwrap();
+    let units = caps.name("units").map_or("px", |u| u.as_str());
+
+    match units.to_lowercase().as_str() {
+        "px" => Mm(quantity * (25.4 / 300.)).into(),
+        "mm" => Mm(quantity).into(),
+        "cm" => Mm(quantity * 10.0).into(),
+        "pt" => Pt(quantity),
+        "in" => Pt(quantity * 72.),
+        "pc" => Pt(quantity * 6.).into(),
+        _ => panic!("Unknown unit types {units}"),
+    }
+}
+
+impl<'a> PageWriter<'a> {
+    pub fn draw_svg(&self, start: Point, svg_text: &str) -> &Self {
+        // let string_to_path_svg = tree.to_string(&usvg::XmlOptions::default());
+        // let text_node = tree.node_by_id("text");
+        let current_layer = self.get_current_layer();
+
+        let svg = Svg::parse(&svg_text).unwrap();
+
+        let svg_ref = svg.into_xobject(&current_layer);
+
+        svg_ref.add_to_layer(
+            &current_layer,
+            SvgTransform {
+                translate_x: Some(start.x),
+                translate_y: Some(start.y),
+                scale_x: Some(1.0),
+                scale_y: Some(1.0),
+                ..Default::default()
+            },
+        );
+
+        let doc = roxmltree::Document::parse(&svg_text).unwrap();
+        for node in doc.descendants().filter(|n| n.tag_name().name() == "text") {
+            let t = node.tag_name();
+
+            // The SVG units are in px by default, and we're assuming that here.
+            //  We have to convert that to Pt which printpdf has a method for, but it
+            //  takes a usize, but the svg pixels can be fractions so... we replicate that
+            //  here
+
+            // TODO: Document/warn that we currently do NOT support text nodes in nested transformations
+            //  OR add support for it.
+            // TODO: Document/warn about unsupported attributes
+            let x = svg_to_pt(node.attribute("x").unwrap_or("0"));
+            let y = svg_to_pt(node.attribute("y").unwrap_or("0"));
+            let weight = FontWeight::from(node.attribute("font-weight").unwrap_or("regular"));
+            let font_style = node.attribute("font-style").unwrap_or("normal");
+            let is_italic = font_style.to_lowercase() == "italic";
+            let font_size = svg_to_pt(node.attribute("font-size").unwrap_or("12"));
+            let fill =
+                color_processing::Color::new_string(node.attribute("fill").unwrap_or("#000000"))
+                    .unwrap()
+                    .get_rgba();
+            let anchor = node.attribute("text-anchor").unwrap_or("start");
+            let font_stack = node.attribute("font-family").unwrap_or("sans-serif");
+
+            let preferred_fonts: Vec<_> = font_stack.split(",").map(|f| f.trim()).collect();
+
+            println!("Font Stack: {:?}", preferred_fonts);
+            // We want to find a font in the stack that matches up to a loaded skia/pdf typeface.
+            //   If we don't find one, default to the first typeface, probably?
+
+            let is_centered = anchor.to_lowercase() == "middle";
+
+            // Once we have the correct typeface found, we should be able to use Skia to get the line_metrics
+            //  for the string and compute what we need to compute for center/end alignment and vertical alignment
+            // TODO: This^^
+
+            let layout = TextLayout::new();
+            if !node.children().all(|n| n.is_text()) {
+                panic!("For <text>, we only support all text child nodes for now");
+            }
+
+            let node_text = node.text().unwrap().trim();
+            let rich = RichText::new(
+                node_text,
+                RichTextStyle {
+                    font_size,
+                    weight,
+                    italic: is_italic,
+                    color: (fill.0 as f32, fill.1 as f32, fill.2 as f32),
+                },
+            );
+
+            let paragraph = layout.compute_paragraph_layout(&rich, Pt(1000.0));
+
+            assert_eq!(paragraph.line_metrics.len(), 1);
+
+            let line_metric = paragraph.line_metrics.first().unwrap();
+
+            println!("{:?}", line_metric);
+
+            // current_layer.set_text_matrix(TextMatrix::Translate(start.x + Pt(x), start.y + Pt(y)));
+            current_layer.set_text_matrix(TextMatrix::Translate(
+                start.x + x.into() - line_metric.width / 2.,
+                start.y + y.into() - ((line_metric.ascent - line_metric.descent) / 2.), // - ((line_metric.ascent - line_metric.baseline) / 2.),
+            ));
+
+            let font_idx = find_font_index_by_style(weight, is_italic);
+            let current_font = &self.writer.fonts[font_idx];
+
+            current_layer.set_font(current_font, Pt::from(font_size).0);
+            current_layer.set_fill_color(Color::Rgb(Rgb::new(fill.0, fill.1, fill.2, None)));
+
+            PageWriter::write_text(&current_layer, &node_text, &layout.typeface);
+        }
+
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_svg_to_pt_px() {
+        // pixels * 72 / DPI = pt
+        assert_eq!(svg_to_pt("50"), Pt(12.0));
+        assert_eq!(svg_to_pt("50px"), Pt(12.0));
+
+        assert_eq!(svg_to_pt("100"), Pt(24.0));
+        assert_eq!(svg_to_pt("100px"), Pt(24.0));
+    }
+
+    #[test]
+    fn test_svg_to_pt_mm() {
+        // Taken from a lookup table
+        assert_eq!(svg_to_pt("50mm"), Pt(141.7322834646));
+        assert_eq!(svg_to_pt("20mm"), Pt(56.6929133858));
+    }
+
+    #[test]
+    fn test_svg_to_pt_cm() {
+        // Taken from a lookup table
+        assert_eq!(svg_to_pt("5cm"), Pt(141.7322834646));
+        assert_eq!(svg_to_pt("2cm"), Pt(56.6929133858));
+    }
+
+    #[test]
+    fn test_svg_to_pt_pt() {
+        // 1:1
+        assert_eq!(svg_to_pt("5pt"), Pt(5.));
+        assert_eq!(svg_to_pt("2pt"), Pt(2.));
+        assert_eq!(svg_to_pt("12pt"), Pt(12.));
+        assert_eq!(svg_to_pt("12.5pt"), Pt(12.5));
+    }
+
+    #[test]
+    fn test_svg_to_pt_in() {
+        // 1:72
+        assert_eq!(svg_to_pt("5in"), Pt(360.));
+        assert_eq!(svg_to_pt("2in"), Pt(144.));
+        assert_eq!(svg_to_pt("12in"), Pt(864.));
+    }
+
+    #[test]
+    fn test_svg_to_pt_pc() {
+        // 1:6
+        assert_eq!(svg_to_pt("5pc"), Pt(30.));
+        assert_eq!(svg_to_pt("2pc"), Pt(12.));
+        assert_eq!(svg_to_pt("12pc"), Pt(72.));
+    }
+    
+    
+    #[test]
+    #[should_panic(expected = "Unknown unit types rem")]
+    fn test_unsupported_unit() {
+      svg_to_pt("5rem");
+    }
+    
+}
